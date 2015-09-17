ccf4348e849b8b9caab1fcee82eefa35b432d5da
chengchiongwah <chengchiongwah@gmail.com>
2015-09-17 21:40:23 +0800

    150917

--- manager.py
+++ manager.py
@@ -1,8 +1,9 @@
+import os
 from flask import Flask
 from flask.ext.script import Manager
 from app import create_app
 
-app = create_app
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 
 if __name__ == '__main__':
