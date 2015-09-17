a22c2c673a5f12ef0e82d7270dd7f52b2ebaeac3
CentOS6.5 <cheng@bogon.localdomain>
2015-09-17 17:59:57 +0800

    150917

---
+++ app/main/views.py
@@ -0,0 +1,7 @@
+from flask import render_template
+from flask.ext.login import login_user, logout_user, login_required 
+from . import main
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
