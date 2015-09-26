4640c358b1f22470148bde14b08f60576f833f6c
chengchiongwah <chengchiongwah@gmail.com>
2015-09-26 17:35:28 +0800

    150926

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
+from flask.ext.mail import Mail
 from config import config
 
 login_manager = LoginManager()
@@ -10,6 +11,7 @@ login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+mail = Mail()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -18,6 +20,7 @@ def create_app(config_name):
 
     bootstrap.init_app(app)
     db.init_app(app)
+    mail.init_app(app)
     login_manager.init_app(app)
 
     from .main import main as main_blueprint
--- config.py
+++ config.py
@@ -4,6 +4,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <sguzch@163.com>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    MAIL_SERVER = 'smtp.163.com'
+    MAIL_PORT = 25
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
     @staticmethod
     def init_app(app):
